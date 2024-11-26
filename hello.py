4b5911e279265c85fcfb1a3ae67349b795945e9e
Yash <titanmattersbot05@gmail.com>
2024-11-26 09:11:09 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+buttons = [[
+                            InlineKeyboardButton('☆ ᴍᴏᴠɪᴇ ʀᴇQᴜᴇꜱᴛ ɢʀᴏᴜᴘ ☆', url="https://t.me/TitanMoviess")
+                        ],[
+                            InlineKeyboardButton("🧠 ʜᴇʟᴘ", callback_data="help"),
+                            InlineKeyboardButton("🔰 ᴀʙᴏᴜᴛ", callback_data="about")
+                        ],[
+                            InlineKeyboardButton("⚡ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ", url="https://t.me/TitanXBots"),
+                            InlineKeyboardButton("⚡ ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ", url="https://t.me/TitanMattersSupport")
+                        ],[
+                            InlineKeyboardButton("🧑‍💻 ᴄᴏɴᴛᴀᴄᴛ ᴏᴡɴᴇʀ", user_id=5356695781),
+                            InlineKeyboardButton("🔐 ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ", url="https://github.com/TitanXBots/FileStore-Bot")
+                        ]] 
