740cbe00ef80b2d4a29859556c0eacf550cf7ee0
Yash <titanmattersbot05@gmail.com>
2024-11-26 09:26:08 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,8 +4,8 @@ buttons = [[
                             InlineKeyboardButton("🧠 ʜᴇʟᴘ", callback_data="help"),
                             InlineKeyboardButton("🔰 ᴀʙᴏᴜᴛ", callback_data="about")
                         ],[
-                            InlineKeyboardButton("⚡ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ", url="https://t.me/TitanXBots"),
-                            InlineKeyboardButton("⚡ ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ", url="https://t.me/TitanMattersSupport")
+                            InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ", url="https://t.me/TitanXBots"),
+                            InlineKeyboardButton("🔍 ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ", url="https://t.me/TitanMattersSupport")
                         ],[
                             InlineKeyboardButton("🧑‍💻 ᴄᴏɴᴛᴀᴄᴛ ᴏᴡɴᴇʀ", user_id=5356695781),
                             InlineKeyboardButton("🔐 ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ", url="https://github.com/TitanXBots/FileStore-Bot")
